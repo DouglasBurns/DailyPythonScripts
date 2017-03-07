2d5804962c2d430b67e8c0f0713b2c09e594e65e
Douglas Burns <db0268@bristol.ac.uk>
2017-03-07 14:57:09 +0000

    Plot bins as part of 00. Add option to plot Res

--- dps/analysis/xsection/00_pick_bins.py
+++ dps/analysis/xsection/00_pick_bins.py
@@ -40,17 +40,152 @@ N^{\text{rec\&gen}}_0
  X | X | X
 '''
 from __future__ import print_function
+from argparse import ArgumentParser
+
+import matplotlib as mpl
+mpl.use( 'agg' )
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+from ROOT import TH1, TCanvas, TLine, gDirectory, TObjArray, TColor, TLegend
 from rootpy import asrootpy
 from rootpy.io import File
-from dps.utils.Calculation import calculate_purities, calculate_stabilities
-from dps.utils.hist_utilities import rebin_2d
-from dps.config.xsection import XSectionConfig
-from argparse import ArgumentParser
-from dps.config.variable_binning import bin_edges_full, minimum_bin_width, nice_bin_width
-from dps.utils.file_utilities import write_data_to_JSON
-from ROOT import TH1, TCanvas, TLine, gDirectory, TObjArray, TColor, TLegend
+import rootpy.plotting.root2matplotlib as rplt
 
+from dps.config.xsection import XSectionConfig
+from dps.config import CMS
+from dps.config.variable_binning import bin_edges_vis, minimum_bin_width, nice_bin_width
+from dps.config.latex_labels import b_tag_bins_latex, variables_latex
+from dps.utils.Calculation import calculate_purities, calculate_stabilities
+from dps.utils.ROOT_utils import get_histogram_from_file
+from dps.utils.file_utilities import make_folder_if_not_exists, write_data_to_JSON
+from dps.utils.hist_utilities import rebin_2d, value_tuplelist_to_hist
 import dps.utils.resolution as rs
+
+from matplotlib import rc
+rc( 'font', **CMS.font )
+rc( 'text', usetex = True )
+
+channels_latex = { 'electron':'e+jets', 'muon':'$\mu$+jets', 'combined':'e+$\mu$+jets combined' }
+
+def plot_binning(args, hist, binning, binning_info, variable, channel):
+
+    output_folder = 'plots/binning/'
+    make_folder_if_not_exists(output_folder)
+    b_tag_bin = '2orMoreBtags'
+    title_template = 'CMS Simulation, $\sqrt{s}$ = %d TeV, %s, %s, %s'
+    title = title_template % ( args.com, channels_latex[channel], '$\geq$ 4 jets', b_tag_bins_latex[b_tag_bin] )
+
+    if 'NJets' in variable:
+        binning = bin_edges_vis['NJets']
+        binned_hist = rebin_2d( hist, binning, binning ).Clone( 'NJet_tmp' )
+        binning_info['p_i'] = calculate_purities(binned_hist)
+        binning_info['s_i'] = calculate_stabilities(binned_hist)
+
+    #########################################################################################################
+    ### Binning
+    #########################################################################################################
+    my_cmap = cm.get_cmap( 'jet' )
+    my_cmap.set_under( 'w' )
+
+    binning_overlay = hist
+    max_bin         = binning_overlay.GetMaximumBin()
+    max_bin_content = binning_overlay.GetBinContent(max_bin)
+    norm            = mpl.colors.LogNorm(vmin = 1, vmax = int(max_bin_content+1))
+
+    x_limits        = [binning[0], binning[-1]]
+    y_limits        = x_limits
+
+    x_title         = 'Reconstructed $' + variables_latex[variable] + '$ [GeV]'
+    y_title         = 'Generated $' + variables_latex[variable] + '$ [GeV]'
+    save_as_name    = 'binning_' + channel + '_' + variable 
+
+    plt.figure( figsize = ( 20, 16 ), dpi = 200, facecolor = 'white' )
+    ax0 = plt.axes()
+    ax0.minorticks_on()
+    ax0.xaxis.labelpad = 12
+    ax0.yaxis.labelpad = 12
+
+    im = rplt.imshow( binning_overlay, axes = ax0, cmap = my_cmap, norm=norm )
+    colorbar = plt.colorbar( im )
+    colorbar.ax.tick_params( **CMS.axis_label_major )
+
+    # draw lines at bin edges values
+    for edge in binning:
+        # do not inclue first and last values
+        if ( edge != binning[0] ) and ( edge != binning[-1] ):
+            plt.axvline( x = edge, color = 'red', linewidth = 4, alpha = 0.5 )
+            plt.axhline( y = edge, color = 'red', linewidth = 4, alpha = 0.5 )
+
+    ax0.set_xlim( x_limits )
+    ax0.set_ylim( y_limits )
+
+    plt.tick_params( **CMS.axis_label_major )
+    plt.tick_params( **CMS.axis_label_minor )
+
+    plt.xlabel( x_title, CMS.x_axis_title )
+    plt.ylabel( y_title, CMS.y_axis_title )
+    plt.title( title, CMS.title )
+
+    plt.tight_layout()
+    plt.savefig( output_folder + save_as_name + '.pdf' )
+
+    #########################################################################################################
+    ### Purity / Stability / Resolution
+    #########################################################################################################
+    plot_res = args.plot_resolution
+
+    hist_purity = value_tuplelist_to_hist(binning_info['p_i'], binning)
+    hist_purity.color = 'red'
+    hist_purity.linewidth = 4
+
+    hist_stability = value_tuplelist_to_hist(binning_info['s_i'], binning)
+    hist_stability.color = 'blue'
+    hist_stability.linewidth = 4
+
+    if not 'NJets' in variable and plot_res:
+        hist_resolution = value_tuplelist_to_hist(binning_info['res'], binning)
+        hist_resolution.color = 'green'
+        hist_resolution.linewidth = 4
+        
+        # *0.5 as bin width should be >= 2*res therefore res/0.5*binwidth < 1 and can put onto other plot
+        bin_width = [(binning[i]-binning[i-1])*0.5 for i in range (1, len(binning))]
+        hist_width = value_tuplelist_to_hist(bin_width, binning)
+        hist_resolution = hist_resolution.divide(hist_resolution, hist_width)
+
+    x_limits = [binning[0], binning[-1]]
+    y_limits = [0,1.1]
+    plt.figure( figsize = ( 20, 16 ), dpi = 200, facecolor = 'white' )
+
+    ax0 = plt.axes()
+    ax0.minorticks_on()
+    ax0.xaxis.labelpad = 12
+    ax0.yaxis.labelpad = 12
+
+    rplt.hist( hist_stability , stacked=False, axes = ax0, cmap = my_cmap, vmin = 1, label = 'Stability' )
+    rplt.hist( hist_purity, stacked=False, axes = ax0, cmap = my_cmap, vmin = 1, label = 'Purity' )
+    if not 'NJets' in variable and plot_res:
+        rplt.hist( hist_resolution, stacked=False, axes = ax0, cmap = my_cmap, vmin = 1, label = '2*Resolution/BinWidth' )
+
+    ax0.set_xlim( x_limits )
+    ax0.set_ylim( y_limits )
+
+    plt.tick_params( **CMS.axis_label_major )
+    plt.tick_params( **CMS.axis_label_minor )
+
+    plt.title( title, CMS.title )
+    x_title = '$' + variables_latex[variable] + '$ [GeV]'
+    plt.xlabel( x_title, CMS.x_axis_title )
+    plt.ylabel( 'AU', CMS.y_axis_title )
+
+    leg = plt.legend(loc=4,prop={'size':40})
+    leg.draw_frame(False)   
+
+    plt.tight_layout()
+
+    save_as_name = 'purityStability_'+channel + '_' + variable
+    plt.savefig( output_folder + save_as_name + '.pdf' )
+    return
 
 def main():
     '''
@@ -60,7 +195,7 @@ def main():
     Step 4: Repeat step 2 & 3 until no mo bins can be created
     '''
     parser = ArgumentParser()
-    parser.add_argument( '-v', 
+    parser.add_argument( '--visiblePS', 
         dest    = "visiblePhaseSpace", 
         action  = "store_true",
         help    = "Consider visible phase space or not" 
@@ -75,9 +210,25 @@ def main():
         action  = "store_true",
         help    = "Recalculate the resolution plots" 
     )
+    parser.add_argument( '-C', 
+        dest    = "com",
+        default = 13, 
+        type    = int,
+        help    = "Centre of mass" 
+    )
+    parser.add_argument( '-v', "--variable",
+        dest    = "variable",
+        default = '', 
+        help    = "Variable to run" 
+    )
+    parser.add_argument( "--plotRes",
+        dest    = "plot_resolution",
+        action  = "store_true",
+        help    = "Plot the resolution" 
+    )
     args = parser.parse_args()
 
-    measurement_config = XSectionConfig(13)
+    measurement_config = XSectionConfig(args.com)
 
     # Initialise binning parameters
     bin_choices = {}
@@ -85,7 +236,6 @@ def main():
     # Min Purity and Stability
     p_min = 0.6
     s_min = 0.6
-    # 0.5 for MET
 
     # Min events in bin for appropriate stat unc
     # error = 1/sqrt(N) [ unc=5% : (1/0.05)^2 = 400]
@@ -94,6 +244,7 @@ def main():
      
     variables = measurement_config.variables
     for variable in variables:
+        if args.variable and args.variable not in variable: continue
         global var
         var=variable
         print('--- Doing variable',variable)
@@ -118,7 +269,7 @@ def main():
         elif variable == 'lepton_pt':
             best_binning, histogram_information = get_best_binning( histogram_information , p_min, s_min, n_min_lepton, minimum_bin_width[variable], nice_bin_width[variable], x_min=26. )
         elif variable == 'abs_lepton_eta':
-            best_binning, histogram_information = get_best_binning( histogram_information , p_min, s_min, n_min_lepton, minimum_bin_width[variable], nice_bin_width[variable] )
+            best_binning, histogram_information = get_best_binning( histogram_information , p_min, s_min, n_min_lepton, minimum_bin_width[variable], nice_bin_width[variable], x_max=2.4 )
         elif variable == 'NJets':
             best_binning, histogram_information = get_best_binning( histogram_information , p_min, s_min, n_min, minimum_bin_width[variable], nice_bin_width[variable], is_NJet=True)
         else:
@@ -153,6 +304,7 @@ def main():
         print('bin edges =', best_binning)
         print('N_bins    =', len( best_binning ) - 1)
         print('The corresponding purities and stabilities are:')
+
         for info in histogram_information:
             outputInfo = {}
             outputInfo['p_i'] = info['p_i']
@@ -162,8 +314,10 @@ def main():
             outputJsonFile = 'unfolding/13TeV/binningInfo_%s_%s_FullPS.txt' % ( variable, info['channel'] )
             if args.visiblePhaseSpace:
                 outputJsonFile = 'unfolding/13TeV/binningInfo_%s_%s_VisiblePS.txt' % ( variable, info['channel'] )
+            plot_binning(args, info['hist'], best_binning, outputInfo, variable, info['channel'])
             write_data_to_JSON( outputInfo, outputJsonFile )
-            print_latex_table(info, variable, best_binning)
+            # print_latex_table(info, variable, best_binning)
+
         for key in outputInfo:
             print (key,outputInfo[key])
         print('-' * 120)
@@ -175,6 +329,7 @@ def main():
     for variable in bin_choices:
         print('\''+variable+'\' : '+str(bin_choices[variable])+',')
 
+    
 def get_histograms( config, variable, args ):
     '''
     Return a dictionary of the unfolding histogram informations (inc. hist)
@@ -233,7 +388,7 @@ def get_histograms( config, variable, args ):
     return histogram_information
 
 
-def get_best_binning( histogram_information, p_min, s_min, n_min, min_width, nice_width, x_min = None, is_NJet=False ):
+def get_best_binning( histogram_information, p_min, s_min, n_min, min_width, nice_width, x_min = None, x_max = None, is_NJet=False ):
     '''
     Step 1: Change the size of the first bin until it fulfils the minimal criteria
     Step 3: Check if it is true for other channel histograms. If not back to step 2
@@ -255,11 +410,15 @@ def get_best_binning( histogram_information, p_min, s_min, n_min, min_width, nic
     if x_min:
         current_bin_start = first_hist.ProjectionX().FindBin(x_min) - 1
         current_bin_end = current_bin_start
-    
+
+    if x_max:
+        end = first_hist.ProjectionX().FindBin(x_max)
+
     # Calculate the bin edges until no more bins can be iterated over
     while current_bin_end < n_bins:
+        if current_bin_end >= end: break
         # Return the next bin end + (p, s, N_reco, res)
-        current_bin_end, _, _, _, r = get_next_end( histograms, current_bin_start, current_bin_end, p_min, s_min, n_min, min_width, nice_width, is_NJet=is_NJet )
+        current_bin_end, _, _, _, r = get_next_end( histograms, current_bin_start, current_bin_end, p_min, s_min, n_min, min_width, nice_width, is_NJet=is_NJet, end=end )
         resolutions.append(r)
 
         # Attach first bin low edge
@@ -278,12 +437,13 @@ def get_best_binning( histogram_information, p_min, s_min, n_min, min_width, nic
         n_events            = [int( get_bin_content( i ) ) for i in range( 1, len( bin_edges ) )]
 
         # Now check if the last bin also fulfils the requirements
+        # Dont merge last eta bins. Should be ok as very high p,s and good res across all. Stops last bin being bigger than due to differences in eta cut
         if ( purities[-1] < p_min or stabilities[-1] < s_min or n_events[-1] < n_min ) and len(purities) > 3:
             # Merge last two bins 
             bin_edges[-2]   = bin_edges[-1]
             bin_edges       = bin_edges[:-1]
             # Merge the resolutions in the last bins
-            resolutions[-2] = (resolutions[-2]+resolutions[-1]) / 2
+            resolutions[-2] = 0.5*(resolutions[-2]+resolutions[-1])
             resolutions     = resolutions[:-1]
             # Recalculate purities and stabilites
             new_hist        = rebin_2d( hist_info['hist'], bin_edges, bin_edges ).Clone()
@@ -304,7 +464,7 @@ def get_best_binning( histogram_information, p_min, s_min, n_min, min_width, nic
 
     return bin_edges, histogram_information
 
-def get_next_end( histograms, bin_start, bin_end, p_min, s_min, n_min, min_width, nice_width, is_NJet=False ): 
+def get_next_end( histograms, bin_start, bin_end, p_min, s_min, n_min, min_width, nice_width, is_NJet=False, end=None ): 
     '''
     Getting the next bin end
     '''
@@ -319,11 +479,14 @@ def get_next_end( histograms, bin_start, bin_end, p_min, s_min, n_min, min_width
 
         # keep the start bin the same but roll the end bin
         for bin_i in range ( current_bin_end, len( reco_i ) + 1 ):
+
             x_high = reco.GetXaxis().GetBinLowEdge(bin_i)
             x_mid  = reco.GetXaxis().GetBinCenter(int( (current_bin_start+current_bin_end)/2 ) )
             x_low  = reco.GetXaxis().GetBinUpEdge(current_bin_start)
             binWidth = x_high - x_low
-
+            if bin_i >= end:
+                current_bin_end = bin_i
+                break
             if binWidth < min_width:
                 current_bin_end = bin_i
                 continue
